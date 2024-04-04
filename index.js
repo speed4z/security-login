d326fb179a8dc50b7e5b76d58b4b2931c992bc5d
speed4z <shardulshinde2812@gmail.com>
2024-04-04 09:34:38 +0530

    change in index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const saltRounds = 10;
 env.config()
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "root",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
